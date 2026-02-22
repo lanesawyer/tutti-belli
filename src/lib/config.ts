6905669769af2de41ea0483ea7b7846fbda289b0
Lane Sawyer <github@lanesawyer.dev>
2026-02-22 11:01:30 -0800

    new top level navigation, fix song display for non-admins

---
+++ src/lib/config.ts
@@ -0,0 +1,16 @@
+export type AdminTool = {
+  href: (ensembleId: string) => string;
+  icon: string;
+  label: string;
+};
+
+export const ensembleAdminTools: AdminTool[] = [
+  { href: (id) => `/ensembles/${id}/edit`, icon: 'fa-edit', label: 'Settings' },
+  { href: (id) => `/ensembles/${id}/members`, icon: 'fa-users', label: 'Members' },
+  { href: (id) => `/ensembles/${id}/songs`, icon: 'fa-music', label: 'Songs' },
+  { href: (id) => `/ensembles/${id}/rehearsals`, icon: 'fa-calendar-check', label: 'Rehearsals' },
+  { href: (id) => `/ensembles/${id}/announcements`, icon: 'fa-bullhorn', label: 'Announcements' },
+  { href: (id) => `/ensembles/${id}/parts`, icon: 'fa-list', label: 'Parts' },
+  { href: (id) => `/ensembles/${id}/seasons`, icon: 'fa-calendar-alt', label: 'Seasons' },
+  { href: (id) => `/ensembles/${id}/groups`, icon: 'fa-users-cog', label: 'Groups' },
+];
