56d3046802eb0f14b08579cab7849d622fc9e345
Lane Sawyer <github@lanesawyer.dev>
2026-02-28 22:56:22 -0800

    card component

--- src/lib/bulma.ts
+++ src/lib/bulma.ts
@@ -1,11 +1,7 @@
-export type Color =
-  | 'black'
-  | 'dark'
-  | 'light'
-  | 'white'
-  | 'primary'
-  | 'link'
-  | 'info'
-  | 'success'
-  | 'warning'
-  | 'danger';
+const COLORS = ['black', 'dark', 'light', 'white', 'primary', 'link', 'info', 'success', 'warning', 'danger'] as const;
+
+export type Color = typeof COLORS[number];
+
+export function isColor(value: string): value is Color {
+  return (COLORS as readonly string[]).includes(value);
+}
